baaa3440bc2a75537e312de03170bfa33b7fcb6c
donghyun0118 <kalim0118@rs-team.com>
2025-10-20 14:23:13 +0900

    fix(server): Swagger 경로 문제 해결

--- news-server/src/config/swagger.ts
+++ news-server/src/config/swagger.ts
@@ -1,9 +1,13 @@
-﻿import swaggerJsdoc from "swagger-jsdoc";
+import path from "path";
+import swaggerJsdoc from "swagger-jsdoc";
 import { swaggerDefinition } from "./swagger-definition";
+
+// dist/config 폴더에서 ../../src/routes/ 경로를 찾도록 수정
+const routesGlob = path.join(__dirname, "../../src/routes/**/*.ts");
 
 const options: swaggerJsdoc.Options = {
   definition: swaggerDefinition,
-  apis: ["./src/routes/**/*.ts"],
+  apis: [routesGlob],
 };
 
 export const specs = swaggerJsdoc(options);
