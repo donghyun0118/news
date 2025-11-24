f43996d9ff2e013a1614580043bc659eea3749b2
donghyun0118 <kalim0118@rs-team.com>
2025-11-24 17:52:13 +0900

    Swagger 경로 및 주석 정리: TypeScript 파싱 오류 해결

--- news-server/src/config/swagger.ts
+++ news-server/src/config/swagger.ts
@@ -4,8 +4,7 @@ import { swaggerDefinition } from "./swagger-definition";
 
 const options: swaggerJsdoc.Options = {
   definition: swaggerDefinition,
-  // dist/config 폴더 기준, 상위로 두 번 올라가서 src/routes를 찾음
-  apis: [path.join(__dirname, "../../src/routes/**/*.ts")],
+  apis: [path.join(process.cwd(), "src/routes/**/*.ts")],
 };
 
 export const specs = swaggerJsdoc(options);
